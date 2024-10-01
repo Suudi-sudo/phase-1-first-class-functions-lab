bd9ee412471a159d021fe04d314b9e9c4a4c0c88
suudi abdisalan <abdisalansucdi@gmail.com>
2024-10-01 16:48:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+return drivers.slice(0, 2);
+};
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Mo']));
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+};
+ 
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ 
+const createFareMultiplier = function(multiplier) {
+    return function (fare) {
+        return fare * multiplier;
+    };
+};
+ 
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+ 
+// Function to select different drivers based on provided function
+function selectDifferentDrivers(drivers, selectFunction) {
+    return selectFunction(drivers); // Calls the provided function with the drivers array
+}
+
+
+
